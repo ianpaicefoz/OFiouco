6ef9adc11601e19fcb6b8c76c3e9774dc9b28483
IanPaice <ianpaicefoz@gmail.com>
2016-04-12 15:46:22 +0200

    p3

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,38 @@ var interstitialAdUnit = "ca-app-pub-4906074177432504/1649035673";
 var isOverlap = true; //true: overlap, false: split
 var isTest = false;
 
+
 document.addEventListener("deviceready", function(){
-	alert("ola!");
-	/*window.admob.showBannerAd('bottom-center', 'SMART_BANNER');*/
+	window.admob.setUp(bannerAdUnit, interstitialAdUnit, isOverlap, isTest);
+	//
+	window.admob.onBannerAdPreloaded = function() {
+		/*alert('onBannerAdPreloaded');*/
+	};
+	window.admob.onBannerAdLoaded = function() {
+		/*alert('onBannerAdLoaded');*/
+	};
+	window.admob.onBannerAdShown = function() {
+		/*alert('onBannerAdShown');*/
+	};
+	window.admob.onBannerAdHidden = function() {
+		/*alert('onBannerAdHidden');*/
+	};	
+	//
+	window.admob.onInterstitialAdPreloaded = function() {
+		alert('onInterstitialAdPreloaded');
+	};
+	window.admob.onInterstitialAdLoaded = function() {
+		alert('onInterstitialAdLoaded');
+	};
+	window.admob.onInterstitialAdShown = function() {
+		alert('onInterstitialAdShown');
+	};
+	window.admob.onInterstitialAdHidden = function() {
+		alert('onInterstitialAdHidden');
+	};
+	
+	window.admob.showBannerAd('bottom-center', 'SMART_BANNER');
+	
 }, false);
 
 
